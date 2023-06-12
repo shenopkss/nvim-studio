55acb16c55fcd138cdbc135f77c3dea4e1d71d34
shenxingshan <shenopkss@qq.com>
2023-06-12 23:46:20 +0800

    update

--- lua/options.lua
+++ lua/options.lua
@@ -33,7 +33,7 @@ if vim.g.neovide then
     vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
     vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
     vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
-    vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+    vim.keymap.set('i', '<D-v>', '<ESC>"+pli') -- Paste insert mode
     vim.g.neovide_scroll_animation_length = 0
     vim.g.neovide_cursor_animation_length = 0
 end
